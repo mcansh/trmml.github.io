fd83286cc4ed1b14615ba08c7e7b1ed7b965a250
Josh Trommel <joshtrommel@gmail.com>
2015-05-03 00:16:25 -0700

    This is a mess. I added Gemfile and fixed up a few things?? idk man

--- main.rb
+++ main.rb
@@ -8,3 +8,22 @@ end
 get '/payment' do
 	slim :payment
 end
+
+post '/charge' do
+  # Amount in cents
+  @amount = 1
+
+  customer = Stripe::Customer.create(
+    :email => 'customer@example.com',
+    :card  => params[:stripeToken]
+  )
+
+  charge = Stripe::Charge.create(
+    :amount      => @amount,
+    :description => 'Sinatra Charge',
+    :currency    => 'usd',
+    :customer    => customer.id
+  )
+
+  erb :charge
+end
