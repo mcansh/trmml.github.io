83cdb14868d2fbc6b38d33d7edcec9bce1da4fbc
Josh Trommel <joshtrommel@gmail.com>
2015-06-21 02:50:29 -0700

    I spent all night on this. R.I.P.

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var repos = JSON.parse(get()).slice(0, 2)
+
+function get() {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open('GET', 'https://api.github.com/users/probablyjosh/repos?sort=pushed', false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+function add(id, index) {
+	document.getElementById(id).innerHTML  = '<a href="' + repos[index].html_url + '">'+ repos[index].name + '</a>'
+}
+
+add('first-repo', 0)
+add('second-repo', 1)
