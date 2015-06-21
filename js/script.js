a2fcaf1f4ef2951c197825cc3b1a7663f3a22742
Josh Trommel <joshtrommel@gmail.com>
2015-06-21 15:13:45 -0700

    Cleaned up and added Last.fm support but still need to fix responsive 'portfolio'

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
-var repos = JSON.parse(get()).slice(0, 2)
+var ghResponse = get('https://api.github.com/users/probablyjosh/repos?sort=pushed')
+var lastfmResponse = get('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=jshtrmml&api_key=0a7d3b25ed857f679eba1a353e98a658&format=json')
+var repos = JSON.parse(ghResponse).slice(0, 2)
+var song = JSON.parse(lastfmResponse)
 
-function get() {
+function get(url) {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open('GET', 'https://api.github.com/users/probablyjosh/repos?sort=pushed', false );
-    xmlHttp.send( null );
+    xmlHttp.open('GET', url, false );
+    xmlHttp.send(null);
     return xmlHttp.responseText;
 }
 
-function add(id, index) {
+function addGH(id, index) {
 	document.getElementById(id).innerHTML  = '<a href="' + repos[index].html_url + '">'+ repos[index].name + '</a>'
 }
 
-add('first-repo', 0)
-add('second-repo', 1)
+addGH('first-repo', 0)
+addGH('second-repo', 1)
+document.getElementById('now-playing').innerHTML = song['recenttracks']['track'][0]['name']
